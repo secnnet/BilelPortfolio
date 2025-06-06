15c47b363dd61877bf0abff04962cf0f0ed97bd3
Bilel <forgithub@sent.com>
2025-06-06 13:39:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/BilelPortfolio/", // 👈 this is the critical part
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,4 +11,3 @@ export default defineConfig({
     },
   },
 })
-
